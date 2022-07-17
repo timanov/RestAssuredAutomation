7c591c3dbd0dac360a16756506de80e0e09a1464
Mikhail <mihailtimanov@gmail.com>
2022-07-17 20:21:14 +0300

    Добавление patch api проверки

--- src/test/java/tests/PutPatchDeleteExamples.java
+++ src/test/java/tests/PutPatchDeleteExamples.java
@@ -35,5 +35,28 @@ public class PutPatchDeleteExamples {
 			statusCode(200)
 			.log().all();
 	}
+	
+	@Test
+	public void testPatch() {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		JSONObject request = new JSONObject();
+		
+		request.put("name","Raghav");
+		request.put("job", "Teacher");
+		
+		baseURI = "https://reqres.in/api";
+		given().
+			header("Content-Type", "application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			patch("/users/2").
+		then().
+			statusCode(200)
+			.log().all();
+	}
 
 }
