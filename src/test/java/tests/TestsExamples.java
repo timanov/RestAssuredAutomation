cf977381d265115ab165101b35a66d5073a67715
Mikhail <mihailtimanov@gmail.com>
2022-07-17 19:01:06 +0300

    add test x5

--- src/test/java/tests/TestsExamples.java
+++ src/test/java/tests/TestsExamples.java
@@ -7,29 +7,35 @@ import static io.restassured.RestAssured.*; //чтобы не писать RestAssured.get, и
 import io.restassured.response.Response;
 
 public class TestsExamples {
-	
+
 	@Test
 	public void test1() {
-		
+
 		Response response = get("https://reqres.in/api/users?page=2");
-		
+
 		System.out.println(response.getStatusCode());
-		System.out.println(response.getTime());	
+		System.out.println(response.getTime());
 		System.out.println(response.getBody().asString());
 		System.out.println(response.getStatusLine());
 		System.out.println(response.getHeader("content-type"));
-		
+
 		int statusCode = response.getStatusCode();
-		
+
 		Assert.assertEquals(statusCode, 200);
 	}
 	
-	
+	@Test
+	public void test_sm_par() {
+		baseURI = "http://sm10.gk.x5.ru";
+		given().get("/api/v1/parameters?page=0&size=10").then().statusCode(200);
+	}
+
 	@Test
 	public void test_2() {
 		baseURI = "https://reqres.in/api/";
 		given().get("users?page=2").then().statusCode(200);
 	}
+	
 	
 
 }
