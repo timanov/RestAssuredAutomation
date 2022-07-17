e8ee19b1c393400462c533aebfef9171bd0cf07e
Mikhail <mihailtimanov@gmail.com>
2022-07-17 12:03:31 +0300

    first commit

---
+++ src/test/java/tests/TestsExamples.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class TestsExamples {
+	
+	@Test
+	public void test1() {
+		
+		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+		
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());	
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+		
+		int statusCode = response.getStatusCode();
+		
+		Assert.assertEquals(statusCode, 200);
+	}
+	
+
+}
