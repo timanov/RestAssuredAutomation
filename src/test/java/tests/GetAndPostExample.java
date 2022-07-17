74a3def88c14d490f7dc9f9c13d95c39cbf0a9f7
Mikhail <mihailtimanov@gmail.com>
2022-07-17 19:59:56 +0300

    add test Post and GET request with Rest Assured

--- src/test/java/tests/GetAndPostExample.java
+++ src/test/java/tests/GetAndPostExample.java
@@ -1,9 +1,13 @@
 package tests;
 
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetAndPostExample {
 	
@@ -18,6 +22,23 @@ public class GetAndPostExample {
 			statusCode(200).
 			body("data[4].first_name", equalTo("George")).
 			body("data.first_name", hasItems("George", "Rachel"));
+	}
+	
+	@Test
+	public void testPost() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+//		map.put("name", "Raghav");
+//		map.put("job", "Teacher");
+//		
+//		System.out.println(map);
+		
+		JSONObject request = new JSONObject();
+		
+		request.put("name","Raghav");
+		request.put("job", "Teacher")
+		
+		System.out.println(request.toJSONString());
 	}
 
 }
